5b0684614cd3fc8438a231b363cb05598f953a6d
Keadon Mitchell <mitchell.keadon@outlook.com>
2022-09-23 21:10:03 -0500

    Add solutions to first 7 problems

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    let arr = string.split("");
+    let output = "";
+    for (let i = arr.length - 1; i >= 0; i--) {
+        output += arr[i];
+    }
 
+    return output;
 };
 
 // Do not edit below this line
